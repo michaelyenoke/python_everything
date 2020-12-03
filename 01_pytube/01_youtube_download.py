0acd274fd07a858e0ce7187790d0dd588e1cbb0e
Y.H. Chen <michaelyenoke@hotmail.com>
2020-12-03 22:14:30 +0800

    Update 01_youtube_download.py

--- 01_pytube/01_youtube_download.py
+++ 01_pytube/01_youtube_download.py
@@ -1,9 +1,9 @@
 from pytube import YouTube
 
-yt=YouTube("https://www.youtube.com/watch?v=VW8AJq6FgxU&feature=youtu.be")
+yt=YouTube("影片網址")
 print("下載Youtube影片\n 影片標題："+yt.title)
 
 GetVideo=yt.streams.filter(file_extension="mp4",resolution="720p").all()
 print(GetVideo)
 stream = GetVideo[0]
-stream.download("C:/Users/MichaelCHEN/Desktop/youtube_download")
+stream.download("下載路徑")
