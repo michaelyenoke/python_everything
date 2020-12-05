0a270738057772f0fac81052de397b42e9e6de18
Y.H. Chen <michaelyenoke@hotmail.com>
2020-12-05 15:21:33 +0800

    Update and rename 01_ to 01_download_comic_book.py

---
+++ 17_web_crawler/01_Selenium/01_download_comic_book.py
@@ -0,0 +1,6 @@
+# [Selenium] 如何使用 Selenium 自動下載漫畫 (1)? : https://www.youtube.com/watch?v=grZi9j4HKvc
+
+from selenium import webdriver
+
+driverPath = 'C:/Users/MichaelCHEN/AppData/Local/Programs/Python/Python37/Scripts/chromedriver.exe'
+browser = webdriver.Chrome(driverPath)
