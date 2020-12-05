4e523e17995a65f8f49258ea9301d895c8327804
Y.H. Chen <michaelyenoke@hotmail.com>
2020-12-05 09:04:23 +0800

    Create 01.doc.py

---
+++ 20_sys/01.doc.py
@@ -0,0 +1,1 @@
+# sys — 你所不知道的 Python 標準函式庫用法 01 : https://blog.louie.lu/2017/07/26/%E4%BD%A0%E6%89%80%E4%B8%8D%E7%9F%A5%E9%81%93%E7%9A%84-python-%E6%A8%99%E6%BA%96%E5%87%BD%E5%BC%8F%E5%BA%AB%E7%94%A8%E6%B3%95-01-sys/
