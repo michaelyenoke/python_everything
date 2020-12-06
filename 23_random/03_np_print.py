02fcaf61d3b7886592be4e94b0fe20a4db61ea65
Y.H. Chen <michaelyenoke@hotmail.com>
2020-12-06 19:50:46 +0800

    Create 03_np_print.py

---
+++ 23_random/03_np_print.py
@@ -0,0 +1,6 @@
+#【python】np.rint() 和 np.ceil() 和 np.linspace()
+# https://blog.csdn.net/weixin_42338058/article/details/83933143
+
+# np.rint()是根据四舍五入取整 <br/>
+# np.ceil()是向上取整，与四舍五入无关 <br/>
+# np.linspace(start, stop, num) 在(start, stop)区间内，选取均匀分布的num个点
