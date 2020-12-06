4392da0e0756de5c798d2a08ba86dd3c2f9e3e29
Y.H. Chen <michaelyenoke@hotmail.com>
2020-12-06 19:38:31 +0800

    Create 01_practice.py

---
+++ 27_smtplib/01_practice.py
@@ -0,0 +1,18 @@
+import smtplib
+
+#mySMTP = smtplib.SMTP('smtp.gmail.com',587)
+#print(type(mySMTP))
+#mySMTP.ehlo() # 250 代表成功,啟動伺服器對話
+#mySMTP.starttls() # 220 代表成功,告知郵件伺服器郵件加密
+
+#mySMTP.login('michaelyenoke@gmail.com','2ouiougi')
+
+
+s = smtplib.SMTP('smtplib.gmail.com',587)
+s.ehlo()
+s.starttls()
+s.login('michaelyenoke@gmail.com','zaleoc2ouiougi')
+try:
+    s.sendmail('michaelyenoke@gmail.com','michaelyenoke@hotmail.com','message_hihi')
+except:
+    print (failed)
