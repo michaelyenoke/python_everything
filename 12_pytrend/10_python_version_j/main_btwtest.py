7c5dba88742ddb847df416df5e5e4180ceeb947c
Y.H. Chen <michaelyenoke@hotmail.com>
2021-02-09 15:28:02 +0800

    Add files via upload

---
+++ 12_pytrend/10_python_version_j/main_btwtest.py
@@ -0,0 +1,219 @@
+#!/usr/bin/python3
+# This Python file uses the following encoding: utf-8
+# https://www.python.org/dev/peps/pep-0263/
+
+from pytrends.request import TrendReq
+from pprint import pprint
+from time import sleep
+from datetime import datetime # import datetime classs from datetime module
+import pandas as pd
+import json
+import csv
+import matplotlib.pyplot as plt
+from numpy.random import randn # 生成隨機數
+import heapq
+import os
+import gcsfs
+import pandas as pd
+from google.cloud import storage
+import numpy as np
+from psutil import virtual_memory
+import warnings  
+
+
+###############
+# 取消警告設定 #
+###############
+
+warnings.filterwarnings("ignore", "Your application has authenticated using end user credentials")
+
+#UserWarning: Your application has authenticated using end user credentials from Google Cloud SDK. We recommend that most server applications use service accounts instead. If your application continues to use end user credentials from Cloud SDK, you might receive a "quota exceeded" or "API not enabled" error. For more information about service accounts, see https://cloud.google.com/docs/authentication/
+# warnings.warn(_CLOUD_SDK_CREDENTIALS_WARNING)
+
+
+#############
+# 記憶體設定 #
+#############
+
+
+#mem = virtual_memory()
+#print(mem)
+#np.zeros((50000,50000), dtype=np.uint8)
+
+
+
+############################
+#### Part 01 - 整理總表 #####
+############################
+
+
+#df=pd.read_csv('gs://openvoice_personalize/personalize_0416_itemid_renew', header=0, low_memory=False)
+#print(df)
+
+data = ['車衣','登山車','腳踏車','滑步車','公路車','折疊車','摺疊車','小徑車','淑女車','旅行車','車褲','袖套','單車','自行車','環法賽','單車環島','自行車環島','自行車安全帽','單車安全帽','腳踏車安全帽','自行車燈','腳踏車燈','自行車訓練台','單車訓練台','滑板車','長板','蛇板','滑板','BMX','越野車','環 台 公路 大賽','環台賽','防曬','反光','太陽眼鏡','墨鏡','萬眾 騎 拜','捷安特','美利達','迪卡儂','童車','兒童腳踏車','圍脖','脖圍','卡鞋','打氣筒','MTB','輔助輪','自行車手機架']
+
+
+pList = []
+
+
+for keywords in data:
+    keywords_list = [keywords]
+    pytrend = TrendReq(hl='en-US', tz=360)
+    pytrend.build_payload(keywords_list, cat=0, timeframe='today 5-y', geo='TW', gprop='')
+    interest_over_time_df = pytrend.interest_over_time()[keywords]
+    pList.append(interest_over_time_df)
+pList_df = pd.DataFrame(pList)
+
+
+# 欄與列的整理
+df3 = pList_df.stack(0)
+df4 = df3.unstack(0)
+# 將index.name的date改成column
+df4.reset_index(inplace=True)
+
+
+
+#
+# 將日期(年,月)單獨抽離出來並與重新合併 #
+#
+
+
+d = []
+
+df4_date = df4['date']
+
+
+for i in range(0,len(df4)):
+    # 將timeframe 轉換成 datetime
+    date_time_obj = datetime.strptime(str(df4_date[i]), '%Y-%m-%d %H:%M:%S') 
+    # 將 datetime 轉換成 string, 並拆開
+    d.append(date_time_obj.strftime("%Y,%m").split())
+
+
+box_1 = []
+box_2 = []
+
+for j in range(0,len(d)):
+    d_sp = d[j][0].split(",",1)
+    d_sp_y = d_sp[0]
+    d_sp_m = d_sp[1]
+    box_1.append(d_sp_y)
+    box_2.append(d_sp_m)
+
+
+box_1_df = pd.DataFrame(box_1)
+box_1_df.rename(columns = {0: "year"},  inplace=True)
+box_2_df = pd.DataFrame(box_2)
+box_2_df.rename(columns = {0: "month"},  inplace=True)
+
+
+clean = pd.concat([box_1_df,box_2_df,df4[data]], axis=1)
+#print(clean)
+#print(clean.columns)
+
+
+
+
+########################################################
+# Part 02  -  整理出 mean / hot_now / diff 表格 by year #
+########################################################
+
+#
+# 計算每一個詞彙過去五年的平均 #
+#
+
+clean_group_mean_year = clean.groupby(['year']).mean()
+clean_mean = clean_group_mean_year.mean()
+clean_mean_2 = round(clean_mean,2)
+clean_mean_2_df = pd.DataFrame(clean_mean_2)
+
+
+#
+# 計算 latest trend
+#
+
+
+clean_last_row  = clean.iloc[-1:]
+clean_last_row_2 = clean_last_row[data] 
+clean_last_row_2_df = clean_last_row_2.stack(0)
+clean_last_row_2_df2 = clean_last_row_2_df.unstack(0)
+
+
+
+#
+# 計算 diff
+#
+
+clean_last_row_2_df2.columns = ['diff']
+clean_mean_2_df.columns =['diff']
+clean_diff = clean_last_row_2_df2 - clean_mean_2_df
+
+
+
+#
+# 再次組合成一個表格
+#
+
+clean_2 = pd.concat([clean_mean_2_df,clean_last_row_2_df2,clean_diff], axis=1, sort=True)
+clean_2.columns = ['mean','now_hot','diff']
+clean_2.index.name = 'label'
+
+
+
+
+
+#########################
+# Part  -  匯出整理 #
+#########################
+
+#
+# 如果要匯入BigQuery要將原本中文欄位取代
+#
+
+
+#clean.rename(columns = {'車衣': 'a01','登山車': 'a02','腳踏車': 'a03','滑步車': 'a04','公路車':'a05','折疊車':'a06','摺疊車':'a07','小徑車':'a08','淑女車':'a09','旅行車':'a10','車褲':'a11','袖套':'a12','單車':'a13','自行車':'a14','環法賽':'a15','單車環島':'a16','自行車環島':'a17','自行車安全帽':'a18','單車安全帽':'a19','腳踏車安全帽':'a20','自行車燈':'a21','腳踏車燈':'a22','自行車訓練台':'a23','單車訓練台':'a24','滑板車':'a25','長板':'a26','蛇板':'a27','滑板':'a28','BMX':'a29','越野車':'a30','環 台 公路 大賽':'a31','環台賽':'a32','防曬':'a33','反光':'a34','太陽眼鏡':'a35','墨鏡':'a36','萬眾 騎 拜':'a37','捷安特':'a38','美利達':'a39','迪卡儂':'a40','童車':'a41','兒童腳踏車':'a42','圍脖':'a43','脖圍':'a44','卡鞋':'a45','打氣筒':'a46','MTB':'a47','輔助輪':'a48','自行車手機架':'a49'},  inplace=True)
+
+#
+# 填補原本index的空白
+#
+
+
+#clean.set_index("year" , inplace=True)
+## print(clean)
+
+
+#
+# 匯出成CSV
+#
+
+googletrend_btw_csv=clean_2.to_csv().encode()
+#print(googletrend_btw_csv)
+print('ok') 
+
+#clean_2.to_csv(path_or_buf='C:/Users/MichaelCHEN/Desktop/googletrend_btw_table_01')
+#print(clean_2)
+
+#
+# 匯到 gcp storage
+#
+
+
+client=storage.Client()
+bucket=client.get_bucket('pytrendtables')
+blob=bucket.blob('googletrend_btw_csv.csv')
+blob.upload_from_string(googletrend_btw_csv)
+print('ok') 
+
+
+
+#######################
+# Part  - Crontab 設定 #
+#######################
+
+
+
+# crontab -e
+# * * * * * /usr/bin/python3 /home/michael_chen/deca_next_pytrend/main_nabajitest.py
+# ctrl o 存檔 - https://man.linuxde.net/nano
+# sudo service cron start
+# sudo service cron status
---
+++ 12_pytrend/10_python_version_j/main_nabaji.py
@@ -0,0 +1,266 @@
+from pytrends.request import TrendReq
+from pprint import pprint
+from time import sleep
+from datetime import datetime # import datetime classs from datetime module
+import pandas as pd
+import json
+import csv
+import matplotlib.pyplot as plt
+from numpy.random import randn # 生成隨機數
+import heapq
+
+
+
+
+#############################
+#### Part One - 整理總表 #####
+#############################
+
+
+data = ['游泳','武漢 肺炎 游泳','泳鏡','蛙鏡','防霧','毛巾','浴巾','泳衣','泳裝','泳褲','比基尼','泳圈','防水包','防水袋','戲水','鼻夾','耳塞','划手板','自由潛水','水中有氧','泳池','仰泳','蛙式','自由式','蛙泳','玩水','冬泳','蝶泳','蛙鞋']
+
+
+pList = []
+
+
+for keywords in data:
+    keywords_list = [keywords]
+    pytrend = TrendReq(hl='en-US', tz=360)
+    pytrend.build_payload(keywords_list, cat=0, timeframe='today 5-y', geo='TW', gprop='')
+    interest_over_time_df = pytrend.interest_over_time()[keywords]
+    pList.append(interest_over_time_df)
+pList_df = pd.DataFrame(pList)
+
+
+# 欄與列的整理
+df3 = pList_df.stack(0)
+df4 = df3.unstack(0)
+# 將index.name的date改成column
+df4.reset_index(inplace=True)
+
+
+
+##################################################
+# Part Two -  將日期(年,月)單獨抽離出來並與重新合併 #
+##################################################
+
+
+d = []
+
+df4_date = df4['date']
+
+
+
+for i in range(0,len(df4)):
+    # 將timeframe 轉換成 datetime
+    date_time_obj = datetime.strptime(str(df4_date[i]), '%Y-%m-%d %H:%M:%S') 
+    # 將 datetime 轉換成 string, 並拆開
+    d.append(date_time_obj.strftime("%Y,%m").split())
+
+
+
+box_1 = []
+
+box_2 = []
+
+
+
+for j in range(0,len(d)):
+    d_sp = d[j][0].split(",",1)
+    d_sp_y = d_sp[0]
+    d_sp_m = d_sp[1]
+    box_1.append(d_sp_y)
+    box_2.append(d_sp_m)
+
+
+
+box_1_df = pd.DataFrame(box_1)
+box_1_df.rename(columns = {0: "year"},  inplace=True)
+box_2_df = pd.DataFrame(box_2)
+box_2_df.rename(columns = {0: "month"},  inplace=True)
+
+
+
+clean = pd.concat([box_1_df,box_2_df,df4[data]], axis=1)
+
+
+
+########################################################
+# Part Three - 整理出 mean / hot_now / diff 表格 by year#
+########################################################
+
+
+
+clean_group_mean_year = clean.groupby(['year']).mean()
+clean_mean = clean_group_mean_year.mean()
+clean_mean_2 = round(clean_mean,2)
+clean_mean_2_df = pd.DataFrame(clean_mean_2)
+
+
+
+clean_last_row  = clean.iloc[-1:]
+clean_last_row_2 = clean_last_row[data] # 去掉前面的 year, month
+clean_last_row_2_df = clean_last_row_2.stack(0)
+clean_last_row_2_df2 = clean_last_row_2_df.unstack(0)
+clean_last_row_2_df2.columns = ['diff']
+clean_mean_2_df.columns =['diff']
+clean_diff = clean_last_row_2_df2 - clean_mean_2_df
+
+
+
+clean_2 = pd.concat([clean_mean_2_df,clean_last_row_2_df2,clean_diff], axis=1)
+clean_2.columns = ['mean','now_hot','diff']
+
+
+
+
+##########################################
+# 在 mean 欄位中, 找出前N位 - 看出長期熱銷#
+##########################################
+
+
+clean_2_mean = clean_2['mean']
+clean_2_mean_dict = dict(clean_2_mean) 
+clean_2_mean_dict_v = clean_2_mean_dict.values() # 取出 dict 的 值
+clean_2_mean_dict_k = clean_2_mean_dict.keys()   # 取出 dict 的 鍵
+
+
+topNum = 10  #選擇top的次數
+nlargestList = heapq.nlargest(topNum,clean_2_mean_dict.values())
+
+
+top_name = []
+year_mean = []
+
+
+for value in nlargestList:
+    for key in clean_2_mean_dict.keys():
+        if  clean_2_mean_dict.get(key) == value: #dict.get()取值  #print(clean_2_mean_dict['陳時中']),結果也會等於 value 
+            top_name.append(key)
+            year_mean.append(value)
+
+
+top_name_df = pd.DataFrame(top_name)   
+year_mean_df = pd.DataFrame(year_mean)
+      
+    
+top_mean_df = pd.concat([top_name_df,year_mean_df], axis=1)
+top_mean_df.columns = ['Name','5年平均']
+
+
+#############################################
+# 在 now_hot 欄位中, 找出前N位 - 看出近期熱銷#
+############################################
+
+
+clean_2_now_hot = clean_2['now_hot']
+clean_2_now_hot_dict = dict(clean_2_now_hot) 
+clean_2_now_hot_dict_v = clean_2_now_hot_dict.values() # 取出 dict 的 值
+clean_2_now_hot_dict_k = clean_2_now_hot_dict.keys()   # 取出 dict 的 鍵
+
+
+topNum = 10  #選擇top的次數
+nlargestList_now_hot = heapq.nlargest(topNum,clean_2_now_hot_dict.values())
+
+
+now_hot_top_name = []
+now_hot_num = []
+
+
+for value in nlargestList_now_hot:
+    for key in clean_2_now_hot_dict.keys():
+        if  clean_2_now_hot_dict.get(key) == value: #dict.get()取值  #print(clean_2_mean_dict['陳時中']),結果也會等於 value 
+            now_hot_top_name.append(key)
+            now_hot_num.append(value)
+
+
+
+now_hot_top_name_df = pd.DataFrame(now_hot_top_name)   
+now_hot_num_df = pd.DataFrame(now_hot_num) 
+now_hot_df = pd.concat([now_hot_top_name_df,now_hot_num_df], axis=1)
+now_hot_df.columns = ['Name','目前熱搜']
+
+
+
+############################################
+# 在 diff 欄位中, 找出前N位 - 看出近期上升趨勢#
+############################################
+
+
+clean_2_diff = clean_2['diff']
+clean_2_diff_dict = dict(clean_2_diff) 
+clean_2_diff_dict_v = clean_2_diff_dict.values() # 取出 dict 的 值
+clean_2_diff_dict_k = clean_2_diff_dict.keys()   # 取出 dict 的 鍵
+
+
+topNum = 10  #選擇top的次數
+nlargestList_diff = heapq.nlargest(topNum,clean_2_diff_dict.values())
+
+
+top_name_diff = []
+year_diff = []
+
+
+for value in nlargestList_diff:
+    for key in clean_2_diff_dict.keys():
+        if  clean_2_diff_dict.get(key) == value: #dict.get()取值  #print(clean_2_mean_dict['陳時中']),結果也會等於 value 
+            top_name_diff.append(key)
+            year_diff.append(value)
+
+
+
+top_name_diff_df = pd.DataFrame(top_name_diff)   
+year_diff_df = pd.DataFrame(year_diff)
+
+
+top_diff_df = pd.concat([top_name_diff_df,year_diff_df], axis=1)
+top_diff_df.columns = ['Name','上升趨勢']
+
+
+
+
+###########################################
+#在 diff 欄位中, 找出後N位 - 看出近期下降趨勢#
+###########################################
+
+
+
+lastNum = 10 
+nsmallest_diff = heapq.nsmallest(lastNum,clean_2_diff_dict.values())
+
+
+name_diff_down = []
+year_diff_down = []
+
+
+
+for value in nsmallest_diff:
+    for key in clean_2_diff_dict.keys():
+        if  clean_2_diff_dict.get(key) == value: #dict.get()取值  #print(clean_2_mean_dict['陳時中']),結果也會等於 value 
+            name_diff_down.append(key)
+            year_diff_down.append(value)
+
+
+name_diff_down_df = pd.DataFrame(name_diff_down)   
+year_diff_down_df = pd.DataFrame(year_diff_down)
+
+down_diff_df = pd.concat([name_diff_down_df,year_diff_down_df], axis=1)
+down_diff_df.columns = ['Name','下降趨勢']
+
+
+
+
+######################################
+#合併 四個 DataFrame 成為一個 DataFram#
+######################################
+
+outcome_1 = []
+outcome_df = [now_hot_df,top_mean_df,top_diff_df,down_diff_df]
+
+
+for out in range(0,len(outcome_df)):
+    outcome_1.append(outcome_df[out])
+    
+print(clean)    
+print(clean_2)
+print(outcome_1)
